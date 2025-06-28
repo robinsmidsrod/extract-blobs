c89aa8952242f891ef5d5350eb7e21dc11f1a623
Robin Smidsrød <robin@smidsrod.no>
2025-06-28 15:25:29 +0200

    Add standard traits to my custom error enum

--- src/error.rs
+++ src/error.rs
@@ -20,3 +20,11 @@ pub enum Error {
     #[from]
     TessVar(leptess::tesseract::TessSetVariableError),
 }
+
+impl std::error::Error for Error {}
+
+impl core::fmt::Display for Error {
+    fn fmt(&self, fmt: &mut core::fmt::Formatter) -> core::result::Result<(), core::fmt::Error> {
+        write!(fmt, "{self:?}")
+    }
+}
